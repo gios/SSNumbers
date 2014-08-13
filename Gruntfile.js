74baef0f86fa2b19ad5f38e03a0270df545839d2
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2014-08-13 23:23:19 +0300

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/js/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    cssmin: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      minify: {
+        src: 'src/css/<%= pkg.name %>.css',
+        dest: 'build/<%= pkg.name %>.min.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+### SSTNumbers: Backbone Project
+SSNumbers - demonstration application;
+
+### SSNumbers: Exploitation
+```
+1) Click on number area - green color;
+2) Right click on number area - red color;
+```
+
+### SSNumbers: Issues
+```
+1) First click doesn't work;
+2) May click on the head of table;
+```
+
+### SSNumbers: Version
+####0.8.1
+---
