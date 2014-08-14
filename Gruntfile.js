29639b4c5f62b486609546c14cc9f50ac1d9bceb
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2014-08-14 20:50:12 +0300

    Delete grunt-contrib-concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,24 +22,13 @@ module.exports = function(grunt) {
         dest: 'build/<%= pkg.name %>.min.css'
       }
     },
-    // concat: {
-    //   options: {
-    //     stripBanners: true,
-    //     banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-    //       '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
-    //   },
-    //   dist: {
-    //     src: ['src/js/config.js', 'src/js/model.js', 'src/js/collection.js', 'src/js/SSNumbers.js'],
-    //     dest: 'build/SSNumbers.js',
-    //   }
-    // },
     requirejs: {
       compile: {
         options: {
           baseUrl: ".",
           mainConfigFile: "src/js/config.js",
           name: "src/js/SSNumbers.js",
-          out: "build/SSNumbers.js"
+          out: "build/SSNumbers.min.js"
         }
       }
     }
@@ -47,7 +36,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   grunt.registerTask('default', ['cssmin', 'requirejs']);
