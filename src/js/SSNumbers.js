3b28076e5d6657988e796e6e53bd65383cd83766
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2014-08-20 18:01:57 +0300

    Optimize model ollection

--- src/js/SSNumbers.js
+++ src/js/SSNumbers.js
@@ -8,135 +8,11 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 
 	var tableCollection = new Collection.TableCollection();
 
-	tableCollection.add(new Model.Table({
-		'first': 1,
-		'second': 2,
-		'third': 3,
-		'fourth': 4,
-		'fifth': 5,
-		'sixth': 6,
-		'seventh': 7,
-		'eighth': 8,
-		'ninth': 9,
-		'tenth': 10
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 11,
-		'second': 12,
-		'third': 13,
-		'fourth': 14,
-		'fifth': 15,
-		'sixth': 16,
-		'seventh': 17,
-		'eighth': 18,
-		'ninth': 19,
-		'tenth': 20
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 21,
-		'second': 22,
-		'third': 23,
-		'fourth': 24,
-		'fifth': 25,
-		'sixth': 26,
-		'seventh': 27,
-		'eighth': 28,
-		'ninth': 29,
-		'tenth': 30
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 31,
-		'second': 32,
-		'third': 33,
-		'fourth': 34,
-		'fifth': 35,
-		'sixth': 36,
-		'seventh': 37,
-		'eighth': 38,
-		'ninth': 39,
-		'tenth': 40
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 41,
-		'second': 42,
-		'third': 43,
-		'fourth': 44,
-		'fifth': 45,
-		'sixth': 46,
-		'seventh': 47,
-		'eighth': 48,
-		'ninth': 49,
-		'tenth': 50
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 51,
-		'second': 52,
-		'third': 53,
-		'fourth': 54,
-		'fifth': 55,
-		'sixth': 56,
-		'seventh': 57,
-		'eighth': 58,
-		'ninth': 59,
-		'tenth': 60
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 61,
-		'second': 62,
-		'third': 63,
-		'fourth': 64,
-		'fifth': 65,
-		'sixth': 66,
-		'seventh': 67,
-		'eighth': 68,
-		'ninth': 69,
-		'tenth': 70
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 71,
-		'second': 72,
-		'third': 73,
-		'fourth': 74,
-		'fifth': 75,
-		'sixth': 76,
-		'seventh': 77,
-		'eighth': 78,
-		'ninth': 79,
-		'tenth': 80
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 81,
-		'second': 82,
-		'third': 83,
-		'fourth': 84,
-		'fifth': 85,
-		'sixth': 86,
-		'seventh': 87,
-		'eighth': 88,
-		'ninth': 89,
-		'tenth': 90
-	}));
-
-	tableCollection.add(new Model.Table({
-		'first': 91,
-		'second': 92,
-		'third': 93,
-		'fourth': 94,
-		'fifth': 95,
-		'sixth': 96,
-		'seventh': 97,
-		'eighth': 98,
-		'ninth': 99,
-		'tenth': 100
-	}));
+	for (var i = 1; i <= 100; i++) {
+		tableCollection.add(new Model.Table({
+			number: i
+		}));
+	}
 
 	//Block right click
 	document.oncontextmenu = function() {
@@ -144,12 +20,12 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 	};
 
 	//Create own event
-	window.vent = {};
+	var vent = {};
 	_.extend(vent, Backbone.Events);
 
 	//Iterator Statistic
-	window.i = 0;
-	window.j = 0;
+	var counterCorrect = 0;
+	var counterIncorrect = 0;
 
 	//Declare View 
 	var TableView = Backbone.View.extend({
@@ -158,9 +34,9 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 
 		initialize: function() {
 			vent.on("TableCorrect", function() {
-				i++;
+				counterCorrect++;
 				$(this.el).find("td").click(function() {
-					if($(this).attr("id") == 'correct' || $(this).attr("id") == 'incorrect') {
+					if ($(this).attr("id") == 'correct' || $(this).attr("id") == 'incorrect') {
 						$(this).click(function() {
 							return false;
 						});
@@ -169,7 +45,7 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 						});
 					} else {
 						$(this).attr("id", "correct");
-						statisticView.render('correct', i);
+						statisticView.render('correct', counterCorrect);
 						$(this).click(function() {
 							return false;
 						});
@@ -184,9 +60,9 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 				console.log("Correct");
 			}, this);
 			vent.on("TableIncorrect", function() {
-				j++;
+				counterIncorrect++;
 				$(this.el).find("td").contextmenu(function() {
-					if($(this).attr("id") == 'correct' || $(this).attr("id") == 'incorrect') {
+					if ($(this).attr("id") == 'correct' || $(this).attr("id") == 'incorrect') {
 						$(this).click(function() {
 							return false;
 						});
@@ -195,7 +71,7 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 						});
 					} else {
 						$(this).attr("id", "incorrect");
-						statisticView.render('incorrect', j);
+						statisticView.render('incorrect', counterIncorrect);
 						$(this).click(function() {
 							return false;
 						});
@@ -214,13 +90,15 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 		render: function() {
 			var viewHtml = '<table border="1">';
 			viewHtml += "<h2>SSNumbers</h2>";
+			var self = this;
 			//Iterate collection
-			_.each(this.collection.models, function(m) {
-				var collectionHtml = '<tr><td class="number">' + m.get('first') + '</td><td class="number">' + m.get('second') +
-					'</td><td class="number">' + m.get('third') + '</td><td class="number">' + m.get('fourth') +
-					'</td><td class="number">' + m.get('fifth') + '</td><td class="number">' + m.get('sixth') +
-					'</td><td class="number">' + m.get('seventh') + '</td><td class="number">' + m.get('eighth') +
-					'</td><td class="number">' + m.get('ninth') + '</td><td class="number">' + m.get('tenth') + '</td></tr>';
+			_.each(self.collection.models, function(m, e) {
+				var collectionHtml = '<td class="number">' + m.get('number') + '</td>';
+				for (var j = 9; j <= self.collection.length; j += 10) {
+					if (e == j) {
+						collectionHtml += '<tr>';
+					}
+				}
 				viewHtml += collectionHtml;
 			});
 
@@ -246,6 +124,7 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 	//Collection to App.Views.Table
 	var tableView = new TableView({
 		collection: tableCollection,
+		model: new Model.Statistic
 	});
 
 	//Render the View
@@ -283,7 +162,6 @@ require(['jquery', 'underscore', 'backbone', 'model', 'collection'], function($,
 	// Random click :)
 	$(document).ready(function() {
 		var random = Math.floor(Math.random() * 100);
-		console.log(random);
 		$('body').find('#' + random).click();
 		$('body').find('#' + random).contextmenu();
 	});
--- src/js/model.js
+++ src/js/model.js
@@ -9,16 +9,7 @@ define('model', ['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 	//Declaring Table Model
 	var Table = Backbone.Model.extend({
 		defaults: {
-			first: 1,
-			second: 2,
-			third: 3,
-			fourth: 4,
-			fifth: 5,
-			sixth: 6,
-			seventh: 7,
-			eighth: 8,
-			ninth: 9,
-			tenth: 10
+			number: 0
 		}
 	});
 
